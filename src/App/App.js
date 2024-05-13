0187764adb0244725cecd2e20f990259960c6d39
Gabrielhzt <hazout26@gmail.com>
2024-05-13 18:25:42 +0200

    Added the grid template and the content in the first box

---
+++ src/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='all-boxes'>
+      <div className='box-1'>
+        <div className='top-logo'>
+          <h1>Jammming</h1>
+          <p>Powered by Spotify</p>
+        </div>
+        <h2 className='title'>Let's find something for your playlist</h2>
+      </div>
+      <div className='box-2'>
+
+      </div>
+      <div className='box-3'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
